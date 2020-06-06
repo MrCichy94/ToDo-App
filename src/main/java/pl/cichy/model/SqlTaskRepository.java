9b946c26a0689d4bd19bc337496db8ef2db6784b
MrCichy94 <przemyslav.cichy@gmail.com>
2020-06-06 20:28:58 +0200

    local profile + flyway

--- src/main/java/pl/cichy/model/SqlTaskRepository.java
+++ src/main/java/pl/cichy/model/SqlTaskRepository.java
@@ -1,9 +1,13 @@
 package pl.cichy.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 interface SqlTaskRepository extends TaskRepository, JpaRepository<Task, Integer> {
 
+    @Override
+    @Query(nativeQuery = true, value ="SELECT count(*) > 0 from tasks where id=?1")
+    boolean existsById(Integer id);
 }
